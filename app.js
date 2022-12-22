c484ca823569a2d200667f720b13fcda3a2a8655
kumkum12345 <asif.kdosha@gmail.com>
2022-12-22 14:29:43 +0200

    mongo DB connection

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const e = require("express")
 const app = express() //initialize express app as constant
 const router = require("./router")
 
+const mongoose =require("mongoose")//init mongoose for better API for mongo
+
+//connect to mongo db
+const dbURI='mongodb+srv://admin:19admin28@travelagencydb.6fhjdmd.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(dbURI).then((result)=> console.log('conection to db was succeful'))
+
 app.use(express.urlencoded({extended :false}))
 app.use(express.json())
 app.use(express.static("views"))
