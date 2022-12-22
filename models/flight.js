534d6fc6b570ca36d687c94e8523f558930b0023
kumkum12345 <asif.kdosha@gmail.com>
2022-12-22 15:12:33 +0200

    Update flight.js

---
+++ models/flight.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema;
+
+
+const flightSchema = new Schema({
+    date: {type: Date,required: true},
+    time: {type: int, required: true},
+    destination:{type: String, required: true},
+    departure:{type: String, required: true},
+    emptySeats: {type: int, required: true}
+}{timestamps: true})
