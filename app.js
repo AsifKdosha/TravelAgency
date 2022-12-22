854e63def828d9620c17e7fb65d3a5788dc95d4b
kumkum12345 <asif.kdosha@gmail.com>
2022-12-22 15:12:08 +0200

    init local host and models update=

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const mongoose =require("mongoose")//init mongoose for better API for mongo
 
 //connect to mongo db
 const dbURI='mongodb+srv://admin:19admin28@travelagencydb.6fhjdmd.mongodb.net/?retryWrites=true&w=majority'
-mongoose.connect(dbURI).then((result)=> console.log('conection to db was succeful'))
+mongoose.connect(dbURI).then(app.listen(3000)).catch((err)=>console.log(err))
 
 app.use(express.urlencoded({extended :false}))
 app.use(express.json())
@@ -18,4 +18,3 @@ app.set("view engine", "html")
 
 app.use("/", router)
 
-app.listen(3000)
